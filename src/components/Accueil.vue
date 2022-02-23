8c0effe376206fd1e00a9fd940577add16f97ea4
delphine <delphin38@live.fr>
2022-02-23 20:19:09 +0100

    mise à jour

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,15 +1,32 @@
 <template>
-  <h1 class="text-center">
+  <h1 class="text-center my-5">
     Stickers et Flocages 69 Vous souhaite la Bienvenue !
   </h1>
+  <h2 class="text-center my-5">
+    Chez Stick flock 69, nous faisons sur mesure et personnalisé vos stickers et
+    flocages. <br />
+    Pour toutes commandes, envoyer un mail à : alice@protonmail.com
+  </h2>
 
-  <h3 class="text-center">Voici quelques projets éffectuer</h3>
+  <h3 class="text-center m-5">Quelques flocages :</h3>
 
   <div class="container">
-    <img src="../assets/a.jpg" />
-    <img src="../assets/c.jpg" />
-    <img src="../assets/e.jpg" />
-    <img src="../assets/f.jpg" />
+    <article>
+      <img src="../assets/sacboule.jpg" />
+      <p>Sac de boule personnalisé</p>
+    </article>
+
+    <article>
+      <img src="../assets/teeshirtpauline.jpg" />
+      <img src="../assets/sacpauline.jpg" />
+      <p>Remerciements pour fin d'année scolaire</p>
+    </article>
+
+    <article>
+      <img src="../assets/teeshirtmecsuper.jpg" />
+      <p>Ce mec .....</p>
+    </article>
+
     <img src="../assets/h.jpg" />
     <img src="../assets/i.jpg" />
     <img src="../assets/j.jpg" />
